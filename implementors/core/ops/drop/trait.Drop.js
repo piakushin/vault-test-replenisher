93a3a687731518ca5715bedc615f64f28fe12d02
piakushin <jetpilot412@gmail.com>
2022-11-04 09:55:33 +0000

    Deploying to gh-pages from @ piakushin/vault-test-replenisher@007832a4d1339f390178919099be4477db250253 🚀

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {
-"near_sdk":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"near_sdk/struct.Promise.html\" title=\"struct near_sdk::Promise\">Promise</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["near_sdk"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"near_sdk/struct.Promise.html\" title=\"struct near_sdk::Promise\">Promise</a>","synthetic":false,"types":["near_sdk::promise::Promise"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/near_contract_standards/fungible_token/receiver/trait.FungibleTokenReceiver.js
+++ implementors/near_contract_standards/fungible_token/receiver/trait.FungibleTokenReceiver.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {
-"vault_test_replenisher":[["impl FungibleTokenReceiver for <a class=\"struct\" href=\"vault_test_replenisher/struct.Contract.html\" title=\"struct vault_test_replenisher::Contract\">Contract</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["vault_test_replenisher"] = [{"text":"impl FungibleTokenReceiver for <a class=\"struct\" href=\"vault_test_replenisher/struct.Contract.html\" title=\"struct vault_test_replenisher::Contract\">Contract</a>","synthetic":false,"types":["vault_test_replenisher::Contract"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
